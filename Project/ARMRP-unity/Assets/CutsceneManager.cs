7a70dbd02eb113797d642418fccf3edb40ffe41c
Panbin <1764183867@qq.com>
2021-11-25 10:30:25 +0800

    Add slate

---
+++ Project/ARMRP-unity/Assets/CutsceneManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Slate;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class CutsceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cutscene cutscene = this.gameObject.GetComponent<Cutscene>();
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
