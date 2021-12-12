ec21f7c163a734043e5e2bd80f5e8fc4b579021e
Panbin <1764183867@qq.com>
2021-12-12 23:45:19 +0800

    Fix FSM

--- Project/ARMRP-unity/Assets/Scripts/Move.cs
+++ Project/ARMRP-unity/Assets/Scripts/Move.cs
@@ -3,23 +3,22 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public Cutscene cutscene;
+    public float speed;
 
     // Start is called before the first frame update
     private void Start()
     {
-        GameObject slateRes = Resources.Load<GameObject>("Slate/run");
-        var slate = GameObject.Instantiate(slateRes);
-        cutscene = slate.GetComponent<Cutscene>();
-        foreach (var cutsceneGroup in cutscene.groups)
-        {
-            cutsceneGroup.actor = this.gameObject;
-        }
+    
     }
 
     // Update is called once per frame
     private void Update()
     {
-        cutscene.Play();
+        //¿ØÖÆÒÆ¶¯
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("a");
+            this.transform.Translate(Vector3.left * speed);
+        }
     }
 }
