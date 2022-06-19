64e2a30e52779231b01c7fabf47aa8b1c02bdc68
Panbin <1764183867@qq.com>
2022-06-19 22:35:03 +0800

    Monster BehaviourTree

---
+++ Project/ARMRP-unity/Assets/Scripts/NodeCanvas/BehaviourNode/BehaviourPlayCutscene.cs
@@ -0,0 +1,13 @@
+﻿using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+namespace NodeCanvas.Tasks.Actions
+{
+
+    [ParadoxNotion.Design.Category("My Nodes/BehaviourTree/PlayCutscene")]
+    class BehaviourPlayCutscene
+    {
+
+    }
+}
